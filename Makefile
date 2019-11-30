6bc9c548f2f9ca59a2aa9503b5cd44d507c43fb7
Sirus Shahini <sirus.shahini@gmail.com>
2019-11-30 11:02:27 -0700

    Added minor formatting improvements. Sync with Whonix.

--- Makefile
+++ Makefile
@@ -4,6 +4,17 @@ pwd := $(shell pwd)/module
 all:
 	gcc -oload load.c
 	cd module; make -C $(build_path) M=$(pwd)
-	
+
 clean:
-	rm load module/tirdad.ko
+	rm -r -f \
+		load
+		module/tirdad.ko \
+		module/modules.order \
+		module/.tirdad.ko.cmd \
+		module/.tirdad.mod.o.cmd \
+		module/.tirdad.o.cmd \
+		module/.tmp_versions \
+		module/Module.symvers \
+		module/tirdad.mod.c \
+		module/tirdad.mod.o \
+		module/tirdad.o
--- module/tirdad.c
+++ module/tirdad.c
@@ -52,139 +52,145 @@ u32 secure_tcp_seq_hooked(__be32 saddr, __be32 daddr,
 	temp = *((u32*)(&seq_secret.key[1]));
 	temp>>=8;
 	last_secret.key[1] += temp;
-	
+
 	hash = siphash_3u32((__force u32)saddr, (__force u32)daddr,
 			        (__force u32)sport << 16 | (__force u32)dport,
-			        &last_secret);	
+			        &last_secret);
 	return hash;
 }
 int store_p_bits(unsigned long address, unsigned char bits){
-	pgd_t *pgd; 
-	pud_t *pud;  
-	pmd_t *pmd;  
-	pte_t *ptep; 
-	p4d_t *p4d;           
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *ptep;
+	p4d_t *p4d;
 	unsigned long ent_val;
 	struct mm_struct *mm;
 
-	unsigned short ps = 1 << 7; 
+	unsigned short ps = 1 << 7;
 	u8 cbit;
 	u8 op_num;
 
-	mm = current->mm; 	
-	pgd = pgd_offset(mm, address); 
+	mm = current->mm;
+	pgd = pgd_offset(mm, address);
 		
 	if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd))){
-		return -1;	
+		return -1;
 	}
-	ent_val = *((unsigned long*)pgd); 
-	op_num = 1;	
+	ent_val = *((unsigned long*)pgd);
+	op_num = 1;
 	cbit = bits & op_num;
-	if (cbit){ 
+	if (cbit){
 	    ent_val = ent_val | 2;
-
 	}else{    
 	    ent_val = ent_val & ~((u8)2);
-
 	}	
-	*((unsigned long*)pgd) = ent_val;  
+	*((unsigned long*)pgd) = ent_val;
    	
-	p4d = p4d_offset(pgd,address);	
-	pud = pud_offset(p4d, address); 
+	p4d = p4d_offset(pgd,address);
+	pud = pud_offset(p4d, address);
 	
-	ent_val = *((unsigned long*)pud);		
-	op_num = 2;	
+	ent_val = *((unsigned long*)pud);
+	op_num = 2;
 	cbit = bits & op_num;
-	if (cbit){ 
+	if (cbit){
 	    ent_val = ent_val | 2;
-	}else{  
+	}else{
 	    ent_val = ent_val & ~((u8)2);
-	}	
-	*((unsigned long*)pud) = ent_val;  
+	}
+	*((unsigned long*)pud) = ent_val;
 	if (!!( ps & *((unsigned long*)pud) ) == 1){
 	    return 1;
 	}
 	pmd = pmd_offset(pud, address);
+	/*
+	 *	We don't have to check for this
+	 *	but if this macro triggers a bug
+	 *	here there's already something wrong
+	 *	with mappings.
+	 *	I leave it to stay here for the
+	 *	sake of completeness.
+	*/
 	VM_BUG_ON(pmd_trans_huge(*pmd));
 	ent_val = *((unsigned long*)pmd);
 
-	op_num = 4;	
+	op_num = 4;
 	cbit = bits & op_num;
-	if (cbit){ 
+	if (cbit){
 	    ent_val = ent_val | 2;
-	}else{    
+	}else{
 	    ent_val = ent_val & ~((u8)2);
-	}	
-	*((unsigned long*)pmd) = ent_val;  
+	}
+	*((unsigned long*)pmd) = ent_val;
 	if (!!( ps & *((unsigned long*)pmd) ) == 1){
 	    return 1;
-	}	
-	ptep=pte_offset_map(pmd, address);	
+	}
+	ptep=pte_offset_map(pmd, address);
 	if (!ptep){
-		return -1;	
+		return -1;
 	}
 	ent_val = *((unsigned long*)(ptep));
 	op_num = 8;
 	cbit = bits & op_num;
-	if (cbit){ 
+	if (cbit){
 	    ent_val = ent_val | 2;
-	}else{    
+	}else{
 	    ent_val = ent_val & ~((u8)2);
-	}	
-	*((unsigned long*)ptep) = ent_val;  
+	}
+	*((unsigned long*)ptep) = ent_val;
 	return 1;
 }
 
 
-int hook_init(void){	
+int hook_init(void){
 	char keys[2][17];
-	int i;   
+	int i;
 	char payload[] = "\x48\xB8\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xE0";
-	u8* payload_adr;    
-	pgd_t *pgd;  	
-	p4d_t *p4d;  
-	pud_t *pud;  
-	pmd_t *pmd;  
+	u8* payload_adr;
+	pgd_t *pgd;
+	p4d_t *p4d;
+	pud_t *pud;
+	pmd_t *pmd;
 	pte_t *ptep; 
 	unsigned long ent_val;
-	struct mm_struct *mm;	
-	unsigned short ps = 1 << 7; 
+	struct mm_struct *mm;
+	unsigned short ps = 1 << 7;
 	u8 cbit;
-    
-	
+
+
 	keys[0][16]=keys[1][16]=0;
-    	
+
 	for (i=0;i<32;i++){
-		keys[i/16][i%16] = _seq_secret[i]; 
+		keys[i/16][i%16] = _seq_secret[i];
 	}
 
  	sscanf(keys[0],"%016lx",(unsigned long*)&seq_secret.key[0]);
  	sscanf(keys[1],"%016lx",(unsigned long*)&seq_secret.key[1]);
 
 	memcpy(&last_secret,&seq_secret,sizeof(seq_secret));
-    
+
 	sscanf(_tcp_secure_seq_adr,"%016lx",&tcp_secure_seq_adr);
 
-    
+
 	p_bits=0;
-       
-	mm = current->mm; 
-	pgd = pgd_offset(mm, tcp_secure_seq_adr); 
-		
+
+	mm = current->mm;
+	pgd = pgd_offset(mm, tcp_secure_seq_adr);
+
 	if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
 		return -1; //EPERM but we use as generic error number
-	
-	ent_val = *((unsigned long*)pgd); 			
+
+	ent_val = *((unsigned long*)pgd);
 	cbit = ent_val & 2;
-	if (cbit) p_bits = 1;	
+	if (cbit) p_bits = 1;
 	p4d = p4d_offset(pgd,tcp_secure_seq_adr);
-		
-	pud = pud_offset(p4d, tcp_secure_seq_adr); 	
+
+	pud = pud_offset(p4d, tcp_secure_seq_adr);
 	ent_val = *((unsigned long*)pud);
 	cbit = ent_val & 2;
 	if (cbit) p_bits = p_bits | 2;
-	
-	if (!!( ps & *((unsigned long*)pud) ) == 1){    
+
+	if (!!( ps & *((unsigned long*)pud) ) == 1){
 	    goto install;
 	}
 
@@ -193,44 +199,44 @@ int hook_init(void){
 	ent_val = *((unsigned long*)pmd);
 	cbit = ent_val & 2;
 	if (cbit) p_bits = p_bits | 4;
-	
+
 	if (!!( ps & *((unsigned long*)pmd) ) == 1){
 	    goto install;
 	}
 
 	ptep=pte_offset_map(pmd, tcp_secure_seq_adr);
-	
+
 	if (!ptep){
 		return -1;
-	
 	}
-	ent_val = *((unsigned long*)(ptep));  
+
+	ent_val = *((unsigned long*)(ptep));
 	cbit = ent_val & 2;
 	if (cbit) p_bits = p_bits | 8;
 
     
     
-install:    
-    
+install:
+
 	store_p_bits(tcp_secure_seq_adr,0x0F);
-      
+
 	payload_adr = (u8*) tcp_secure_seq_adr;
 	memcpy(backup_bytes,(void*)tcp_secure_seq_adr,12);
-	memcpy((void*)tcp_secure_seq_adr,payload,12);   
+	memcpy((void*)tcp_secure_seq_adr,payload,12);
 	*((unsigned long*)&payload_adr[2]) = (unsigned long)&secure_tcp_seq_hooked;
 
-	store_p_bits(tcp_secure_seq_adr,p_bits);       
-    
+	store_p_bits(tcp_secure_seq_adr,p_bits);
+
 	printk("[>] Installing tirdad hook succeeded.\n");
-    
+
 	return 0;
 }
 
-void hook_exit(void){  
-	store_p_bits(tcp_secure_seq_adr,0x0F);    
-	memcpy((void*)tcp_secure_seq_adr,backup_bytes,12);       
-	store_p_bits(tcp_secure_seq_adr,p_bits);  
-    
+void hook_exit(void){
+	store_p_bits(tcp_secure_seq_adr,0x0F);
+	memcpy((void*)tcp_secure_seq_adr,backup_bytes,12);
+	store_p_bits(tcp_secure_seq_adr,p_bits);
+
 	printk("[>] Removed tirdad hook successfully\n");
 }
 module_init(hook_init);
